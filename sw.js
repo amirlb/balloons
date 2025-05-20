a20297ebd02d5bb551e0aec31eb651d083124f81
Amir Livne Bar-on <amir.livne.baron@gmail.com>
2025-05-20 23:09:22 +0300

    service worker

--- sw.js
+++ sw.js
@@ -1,24 +1,30 @@
-const CACHE_NAME = 'balloons-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/script.js',
-  '/assets/icon.png',
-  '/assets/Aylex - Happy Day (freetouse.com).mp3',
-  '/assets/sharp-pop-328170.mp3'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open('v1').then(function (cache) {
+      return cache.addAll([
+        '/balloons/index.html',
+        '/balloons/styles.css',
+        '/balloons/script.js',
+        '/balloons/icon.png',
+        '/balloons/assets/Aylex - Happy Day (freetouse.com).mp3',
+        '/balloons/assets/sharp-pop-328170.mp3'
+      ]);
+    })
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.open('v1').then(async function(cache) {
+      const cache_response = await cache.match(e.request);
+      if (cache_response) {
+        return cache_response;
+      }
+      else {
+        const response = fetch(e.request);
+        await cache.put(e.request, (await response).clone());
+        return response;
+      }
+    })
   );
-}); 
+});
